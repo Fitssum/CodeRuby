0e267522aade3987195a5c50cc3c433fc9c93695
Fitssum Haile <fitsum.wolde@gmail.com>
2017-07-19 19:25:15 -0400

    generated pipe length to limit the size of text in home component

--- coderubynet/src/app/app.module.ts
+++ coderubynet/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { EventService } from './event.service';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { LengthPipe } from './length.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    LengthPipe
   ],
   imports: [
     BrowserModule,
